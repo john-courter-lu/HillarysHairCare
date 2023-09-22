d4d69cd10a1b55914772fb8076d63c4567b586ad
John Lu <johnlu2006@gmail.com>
2023-09-22 11:44:39 -0500

    routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="appointments">
+          <Route index element={"AppointmentList"} />
+          <Route path=":id" element={"AppointmentDetail" } />
+          <Route path="create" element={"AppointmentCreate" } />
+        </Route>
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
