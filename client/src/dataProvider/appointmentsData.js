8c3a3ea7d3c60c6b165b93f08eafbdf544d1c912
John Lu <johnlu2006@gmail.com>
2023-09-26 15:28:13 -0500

    dataProvider with error handling

--- client/src/dataProvider/appointmentsData.js
+++ client/src/dataProvider/appointmentsData.js
@@ -16,3 +16,25 @@ export const createAppointment = (appointment) => {
   }).then((res) => res.json());
 };
 
+export const cancelAppointment = (id) => {
+  try {
+    const response = fetch(`${_apiUrl}/cancel/${id}`, {
+      method: "DELETE",
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error removing appointment (Status ${response.status})`);
+    }
+
+    return true; // Return true if the removal was successful
+  
+  } catch (error) {
+
+    console.error("Error removing appointment:", error);
+    throw error;
+
+  }
+};
+// 这个的error catching是最全面的了, 既有!response.ok 又有error. 
+// 同时有  return true; 不知是否有实质影响; 反正不用也行
+// 还可以改进: 用 = async(id) 和 response = await fetch
