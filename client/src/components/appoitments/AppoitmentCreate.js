723b9cbb1cfccb7968998035989f686b83a14897
John Lu <johnlu2006@gmail.com>
2023-09-26 21:56:49 -0500

    updating button style to align with the syntax

--- client/src/components/appoitments/AppoitmentCreate.js
+++ client/src/components/appoitments/AppoitmentCreate.js
@@ -138,7 +138,7 @@ export default function AppointmentCreate() {
 
         </FormGroup>
 
-        <Button outline="ture" color="primary" block="ture" onClick={submit}>Submit</Button>
+        <Button outline color="primary" block onClick={submit}>Submit</Button>
       </Form>
     </div>
   );
